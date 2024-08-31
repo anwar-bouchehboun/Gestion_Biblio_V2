b4c14f3618bbffae46fbe735271c96c9443fd243
anouar <anouar.ab95@gmail.com>
2024-08-31 21:10:02 +0100

    update dgsin

--- src/main/java/org/example/CoucheMetier/Interface/Implemnation/MagasinImp.java
+++ src/main/java/org/example/CoucheMetier/Interface/Implemnation/MagasinImp.java
@@ -17,20 +17,20 @@ public class MagasinImp implements Bebliothique<Magasin> {
     @Override
     public void add(Magasin document) {
         Magasins.add(document);
-        System.out.println(Magasins);
+        System.out.println(CostumColor.WHITE_BOLD_BRIGHT+Magasins+ CostumColor.RESET);
         listeMapping.put(document.getId(), document);
     }
 
     @Override
     public void emprunt(String titre) {
         Optional<Magasin> doc = getDoc(titre);
-        doc.ifPresentOrElse(Magasin::emprunter, () -> System.out.println("Magasin introuvable."));
+        doc.ifPresentOrElse(Magasin::emprunter, () -> System.out.println(CostumColor.WHITE_BOLD_BRIGHT+"Magasin introuvable."+ CostumColor.RESET));
     }
 
     @Override
     public void retourner(String titre) {
         Optional<Magasin> doc = getDoc(titre);
-        doc.ifPresentOrElse(Magasin::retourner, () -> System.out.println("Magasin introuvable."));
+        doc.ifPresentOrElse(Magasin::retourner, () -> System.out.println(CostumColor.WHITE_BOLD_BRIGHT+"Magasin introuvable."+ CostumColor.RESET));
     }
 
     @Override
@@ -42,7 +42,7 @@ public class MagasinImp implements Bebliothique<Magasin> {
 
     public Optional<Magasin> RechDoc(String titre) {
         Optional<Magasin> doc = getDoc(titre);
-        doc.ifPresentOrElse(Magasin::afficherDetailsMagsin, () -> System.out.println("Magasin introuvable."));
+        doc.ifPresentOrElse(Magasin::afficherDetailsMagsin, () -> System.out.println(CostumColor.WHITE_BOLD_BRIGHT+"Magasin introuvable."+ CostumColor.RESET));
         return doc;
     }
 
